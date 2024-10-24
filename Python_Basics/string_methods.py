0e46399a9e26c22e35642dc5bcaf01a3a5b1b5c0
kesenaya <nanayaakesewaa1010@gmail.com>
2024-10-24 13:22:41 -0700

    pythonAssingment

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,10 @@
+message="Hello, my name is kesanaya"
+print(message. lower())
+message="Hello, my name is cannibal"
+print(message. upper())
+message="Hello, my name is cannibal"
+#replace('cannibal','kesanaya')
+print(message.replace('cannibal','kesanaya'))
+female='vicky'
+male='theo'
+print (female+male)
