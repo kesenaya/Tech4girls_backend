4b9e6787bac51759ca6850ba3b8d50f10c294fb2
kesenaya <nanayaakesewaa1010@gmail.com>
2024-10-28 02:03:30 -0700

    ASSING8

---
+++ Python_Control_Flow/list_and_set_methods.py
@@ -0,0 +1,35 @@
+"""name =["Pat", "Sam", "Nick", "Ken" Nick"]
+#APPEND
+name.append ("Ben")
+print(name)
+#POP
+name.pop()
+print(name)
+#SORT
+name.sort()
+print(name)
+#REVERSE
+name.reverse()
+print(name)
+name2 =("Ted","Nat")
+name.extend(name2)
+print(name)"""
+galaxy ={"sun", "moon","star","comets","air"}
+#ADD
+galaxy.add("planet")
+print(galaxy)
+#REMOVE
+galaxy.remove("sun")
+print(galaxy)
+#UNION
+earth=["tree", "sea,", "air"]
+world= galaxy.union("earth")
+print(world)
+#INTERSECTION
+common = galaxy.intersection("earth")
+print(common)
+#difference
+unique = galaxy.difference("earth")
+print(unique)
+
+
